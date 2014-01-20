a6e681e212eb9c5f04db59e0cb8d7e8211d0f4a5
Seikun Kambashi <seikun_lee@hotmail.com>
2014-01-20 01:16:28 -0500

    ajax practice, working menu

---
+++ example-ajax/beta/android.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    loadPage();
+});
+
+function loadPage(url){
+    if(url == undefined){
+        $('#container').load('index.html #header ul', hijackLinks);
+    }else{
+        $('#container').load(url + '#content', hijackLinks);
+    }
+}
+
+function hijackLinks(){
+    $('#container a').click(function(e){
+        e.preventDefault();
+        loadPage(e.target.href);
+    });
+}
